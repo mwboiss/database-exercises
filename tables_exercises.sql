3cd09d85f0882ba0b7e0a1640471054fba90255a
Mathias W Boissevain <boissevain.mathias@gmail.com>
2022-01-06 16:30:25 -0600

    Missed some data types. Had to scroll out on mysql but it was easier to read when done in the CLI

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,4 +1,4 @@
-#1 and 2 are procedural
+#1 and 2 are procedural.
 
 #3
 USE employees;
@@ -9,7 +9,7 @@ SHOW TABLES;
 #5
 SELECT * from employees;
 SHOW CREATE TABLE employees;
-# emp_no is an int,birth_date is a date,first_name is a varchar(14),last_name is an varchar(16).
+# emp_no:int, birth_date:date, first_name:varchar(14), last_name:varchar(16), gender:enum('M','F'), hire_date:date.
 
 #6 Numeric types are most likely in most of them as id's but most likely in salaries.
 
@@ -17,7 +17,7 @@ SHOW CREATE TABLE employees;
 
 #8 Date types are most likely in most of them but current_dept_emp, dept_emp_latest_date are the most likely suspects.
 
-#9 they both have id's that can be used with dept_emp table to relate employees to their respective departments
+#9 they both have id's that can be used with dept_emp table to relate employees to their respective departments.
 
 #10
 SHOW CREATE TABLE dept_manager;
