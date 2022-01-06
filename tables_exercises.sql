89835277fd4f0b678ff41137dbe38b019e6eca98
Mathias W Boissevain <boissevain.mathias@gmail.com>
2022-01-06 16:03:22 -0600

    Tables exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,23 @@
+#1 and 2 are procedural
+
+#3
+USE employees;
+
+#4
+SHOW TABLES;
+
+#5
+SELECT * from employees;
+SHOW CREATE TABLE employees;
+# emp_no is an int,birth_date is a date,first_name is a varchar(14),last_name is an varchar(16).
+
+#6 Numeric types are most likely in most of them as id's but most likely in salaries.
+
+#7 String types are most likely in all of them as names of employees or departments.
+
+#8 Date types are most likely in most of them but current_dept_emp, dept_emp_latest_date are the most likely suspects.
+
+#9 they both have id's that can be used with dept_emp table to relate employees to their respective departments
+
+#10
+SHOW CREATE TABLE dept_manager;
