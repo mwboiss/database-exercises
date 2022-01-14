76268b6b87f091cc54cf075ef11d1cb8e00f545b
Mathias W Boissevain <boissevain.mathias@gmail.com>
2022-01-14 10:16:20 -0600

    Temp tables submission

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -1,3 +1,5 @@
+#Temporary Tables Exercise
+
 USE employees;
 
 #Problem 1
@@ -21,12 +23,11 @@ ALTER TABLE employees_with_departments DROP COLUMN  first_name;
 ALTER TABLE employees_with_departments DROP COLUMN  last_name;
 
 #D
+#When we created the table we could have brought the names over concatenated with the alias of full name.
 
-
-#Helpers
+#Helpers functions
 DESCRIBE employees_with_departments;
 SELECT * FROM employees_with_departments;
-
 DROP TABLE employees_with_departments;
 ALTER TABLE employees_with_departments DROP COLUMN full_name;
 
@@ -40,7 +41,34 @@ CREATE TEMPORARY TABLE innis_1658.temp_payment_from_sakila AS
 USE innis_1658;
 
 SELECT * FROM temp_payment_from_sakila;
+DESCRIBE temp_payment_from_sakila;
 
-ALTER TABLE temp_payment_from_sakila MODIFY amount amount * 100 INT();
+ALTER TABLE temp_payment_from_sakila MODIFY amount decimal(10,2);
+UPDATE temp_payment_from_sakila SET amount = amount * 100;
+ALTER TABLE temp_payment_from_sakila CHANGE COLUMN amount amount_in_cents int(10);
 
+#Problem 3
+USE employees;
+DESCRIBE departments;
 
+CREATE TEMPORARY TABLE innis_1658.current_vs_historical_avg_salary AS
+	SELECT d.dept_name, 
+				AVG(hs.salary) historical_avg, 
+				AVG(cs.salary) current_avg,
+    		 (AVG(hs.salary) - (SELECT AVG(salary) FROM salaries))/
+    		 (SELECT stddev(salary) FROM salaries) historical_zscore,
+    				(AVG(cs.salary) - (SELECT AVG(salary) FROM salaries WHERE to_date >NOW()))/
+    			(SELECT stddev(salary) FROM salaries WHERE to_date > NOW()) current_zscore
+
+		FROM salaries hs
+			JOIN dept_emp de USING(emp_no)
+			JOIN departments d USING(dept_no)
+			JOIN (
+					SELECT salary, emp_no
+					FROM salaries
+					WHERE to_date > NOW()) cs USING(emp_no)
+		GROUP BY d.dept_name;
+		
+SELECT * FROM current_vs_historical_avg_salary ORDER BY current_zscore;
+
+#From my interpretation of the salary data Sales would be the best department to work for and Human Resources would be the worst. Not only does Sales have the highest current and historical averages but they consistently pay over 1 standard deviation above the mean. Human resources on the other hand have the lowest averages and pay well below the mean of the company. 
